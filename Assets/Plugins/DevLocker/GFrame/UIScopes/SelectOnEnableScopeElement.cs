7c70dd00a95a313821bfbe482b6785de096395fd
Filip Slavov <NibbleByte3@gmail.com>
2021-05-23 11:19:20 +0300

    * SelectOnEnableScopeElement now has an option to persist its selection.

--- Assets/Plugins/DevLocker/GFrame/UIScopes/SelectOnEnableScopeElement.cs
+++ Assets/Plugins/DevLocker/GFrame/UIScopes/SelectOnEnableScopeElement.cs
@@ -8,10 +8,26 @@ namespace DevLocker.GFrame.UIScope
 	/// </summary>
 	public class SelectOnEnableScopeElement : MonoBehaviour, IScopeElement
 	{
+		[Tooltip("Start by selecting current object, but remember what the selection was on disable.\nOn re-enabling, resume from that selection.")]
+		public bool PersistentSelection = false;
+
+		private GameObject m_PersistedSelection = null;
+
 		void OnEnable()
 		{
 			if (EventSystem.current) {
-				EventSystem.current.SetSelectedGameObject(gameObject);
+				if (m_PersistedSelection && m_PersistedSelection.activeInHierarchy) {
+					EventSystem.current.SetSelectedGameObject(m_PersistedSelection);
+				} else {
+					EventSystem.current.SetSelectedGameObject(gameObject);
+				}
+			}
+		}
+
+		void OnDisable()
+		{
+			if (EventSystem.current && PersistentSelection) {
+				m_PersistedSelection = EventSystem.current.currentSelectedGameObject;
 			}
 		}
 	}
